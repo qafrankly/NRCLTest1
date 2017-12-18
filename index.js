674749b838aba68925dff1eb3a4fc26264eef1d5
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-12-18 16:04:53 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+[
+    {
+        "type": "row",
+        "gutterSpacing": "",
+        "cols": [
+            {
+                "type": "col",
+                "spans": {
+                    "xl": 12
+                },
+                "gutterSpacing": "",
+                "components": [
+                    {
+                        "id": "components/news/CardList",
+                        "FRN_resourceEndpoints": [
+                            {
+                                "url": "http://wnow.dsys1.worldnow.com/category/330204/nrcategorytestd1?clienttype=container.json",
+                                "type": "json"
+                            }
+                        ],
+                        "FRN_style": {},
+                        "visualInfo": {
+                            "affiliate": "wnow"
+                        },
+                        "props": {
+                            "startArticleIndex": 0,
+                            "numberOfItems": 7,
+                            "widthStyle": "md",
+                            "heightStyle": "md",
+                            "showThumbnails": true,
+                            "thumbnailAlignment": "left",
+                            "videoIconPlacement": "image-center",
+                            "backgroundColor": "#2c3e50",
+                            "textColor": "#FFFFFF",
+                            "showDescriptions": true,
+                            "showPlayButtonForVideo": true,
+                            "showTimestamp": true,
+                            "showBylines": true,
+                            "timestampOptions": {
+                                "showElapsedTime": true,
+                                "displayShortDateTime": true
+                            },
+                            "showNumbers": false,
+                            "maintainImageAspectRatio": true,
+                            "imageAspectRatioBackgroundColor": "#7f8c8d",
+                            "enableDivider": true,
+                            "dividerOptions": {
+                                "height": "xs",
+                                "backgroundColor": "#CCA43B",
+                                "position": {
+                                    "aboveList": false,
+                                    "belowList": true
+                                }
+                            },
+                            "showReadMore": true,
+                            "categoryUrl": "/sports",
+                            "readMoreAlignment": "right",
+                            "readMoreOptions": {
+                                "showMoreArrow": true,
+                                "buttonStyle": "solid",
+                                "outlineColorStyle": "attention",
+                                "textColorStyle": "attention",
+                                "backgroundColorStyle": "accent",
+                                "urlTarget": "_blank",
+                                "textColor": "#fff",
+                                "outlineColor": "#ccc",
+                                "backgroundColor": "#007aff"
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+]
