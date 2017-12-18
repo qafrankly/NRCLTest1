6e5a8f517fde4bbade9d36717014739fb4501313
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-12-18 16:27:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import React, { Component } from 'react';
+
+class HomePageTakeover extends Component {
+
+  constructor(props){ //gives us acces to props, fires long before page load
+    super(props) //assigns props to this.props
+    this.state = {} /* great place to assign default state */
+  }
 [
     {
         "type": "row",
